d226f486a9f5bddf64eb32b9310737c47d54b08a
Akhil Arjun <akhilparjun@gmail.com>
2016-05-26 20:21:53 +0530

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -7,6 +7,14 @@ var _timeToRead = function(noOfWords){
     return timeReqd;
 };
 
+//Finding Parent browserwise
+var _parentElem = null;
+if((browser.name == 'IE' || browser.name == 'MSIE' || browser.name == 'Firefox')){
+    _parentElem = jQuery('html');
+} else {
+    _parentElem = jQuery('body');
+}
+
 //Progress bar Load
 var loadProgressBar = function(){
     jQuery('.progress-bar').each(function(index,el){
@@ -43,17 +51,43 @@ jQuery('.social-icon').click(function(e){
 });
 
 //Scroll Effect
+var progressBarLoaded = false;
+var scrolled = false;
 jQuery(window).bind('mousewheel DOMMouseScroll touchmove scroll',function(e){
     var heroSection = jQuery('.hero-section');
+    var windowWidth = jQuery(this).width();
+    var scrollTopLimit = _parentElem.scrollTop();
     setTimeout(function(){
-        if(!heroSection.hasClass('aside') && jQuery('body').scrollTop() > 0){
-            heroSection.addClass('aside');
-            loadProgressBar();
-        } else if(jQuery('body').scrollTop() == 0){
-            heroSection.removeClass('aside');
+        if(windowWidth > 600){
+            if(!heroSection.hasClass('aside') && scrollTopLimit > 0){
+                heroSection.addClass('aside');
+                loadProgressBar();
+                if(!scrolled){
+                    _parentElem.animate({
+                        scrollTop: '100px'
+                    });
+                }
+            } else if(jQuery('body').scrollTop() == 0){
+                //heroSection.removeClass('aside');
+            }
+        } else {
+            if(!progressBarLoaded){
+                progressBarLoaded = true;
+                loadProgressBar();
+            }
         }
     },1);
 });
+
+//click Scroll here
+var setAside = function(){
+    var heroSection = jQuery('.hero-section');
+    var windowWidth = jQuery(this).width();
+    if(windowWidth > 600){
+        heroSection.addClass('aside');
+        loadProgressBar();
+    }
+};
 
 //Type Effect
 jQuery('#typing-letters').typed({
@@ -62,5 +96,17 @@ jQuery('#typing-letters').typed({
                "^500 And also...",
                "^500 <strong>Web Developer</strong>"],
     typeSpeed : 0,
-    loop : false
+    loop : true
+});
+
+//Navigation
+jQuery('.menu').click(function(){
+    var idToScroll = jQuery(this).attr('href');
+    if(idToScroll){
+        var elemToScrollTo = jQuery(idToScroll);
+        scrolled = true;
+        _parentElem.animate({
+            scrollTop : elemToScrollTo.offset().top+'px'
+        },500);
+    }
 });
