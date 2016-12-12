a5ba3a4f52fdb982d0297f7756e5f170d56113c9
Akhil Arjun <akhilparjun@gmail.com>
2016-12-12 16:05:10 +0530

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -7,6 +7,11 @@ var _timeToRead = function(noOfWords){
     return timeReqd;
 };
 
+//Trying to register service worker
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/js/sw.js');
+}
+
 //Finding Parent browserwise
 var _parentElem = null;
 if((browser.name == 'IE' || browser.name == 'MSIE' || browser.name == 'Firefox')){
